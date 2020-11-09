21ec50c39994ee6601f3f9169563f509e7ac3c04
jalacardio <mustkeys@gmail.com>
2020-11-09 23:31:20 +0800

    add single program
    allow adding single program with merely list of word which search word and definition for added vocabs

---
+++ ppp/program/services/add_single_program.py
@@ -0,0 +1,36 @@
+from program.models import Vocabulary, VocabularyProgram, VocabularyTranslation
+from dictionary.models import Word
+import random
+
+# value = random()
+
+name = "toefl"
+program_name = "Toefl"
+
+vp = VocabularyProgram.objects.create(name=program_name)
+
+with open("program/services/programs/toefl.txt", "r") as f:
+    for vocab in f:
+        vocab = vocab.strip()
+        w = Word.objects.prefetch_related('translations', 'translations__speeches').get(rep=vocab)
+        v = Vocabulary.objects.create(vocab_program=vp, rep=vocab)
+        first_translation = w.translations.first()
+        speech = first_translation.speeches.first()
+        vt = VocabularyTranslation.objects.create(rep=first_translation.rep, speech=speech, vocabulary=v)
+        print('{} - {} - {}'.format(vt.vocabulary.rep, vt.rep, vt.speech))
+
+# vb = VocabularyBank.objects.prefetch_related('vocabularies', 'vocabularies__word', 'vocabularies__word__translations', 'vocabularies__word__translations__speeches').get(name=name)
+
+#
+# vocabularies = list(vb.vocabularies.all())
+
+# for vocabulary in vocabularies:
+#     vocabulary = vocabularies.pop()
+#     vocabulary.vocab_program = vp
+#     vocabulary.save()
+#     first_translation = vocabulary.word.translations.first()
+#     speech = first_translation.speeches.first()
+#     vt = VocabularyTranslation.objects.create(rep=first_translation.rep, speech=speech, vocabulary=vocabulary)
+#     print('{} - {} - {}'.format(vt.vocabulary.word.rep, vt.rep, vt.speech))
+
+
